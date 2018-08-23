de2e810367dd1bb1238906948c3cbcd26694fd30
root <root@localhost.localdomain>
2018-08-23 14:15:14 +0800

    updates

---
+++ update.sh
@@ -0,0 +1,4 @@
+python3.6 /root/AUTOMATION/Productions_updates/EZ_PZ_3D_update.py
+python3.6 /root/AUTOMATION/Productions_updates/EZ_PZ_3D_RU_update.py
+python3.6 /root/AUTOMATION/Productions_updates/EZ_PZ_RPG_update.py
+python3.6 /root/AUTOMATION/Productions_updates/Prductions_excel_output.py
